74455de3e953ea4eb5062f6f676493291dfcda9f
tomasyarce2007 <toyartej@gmail.com>
2022-03-17 17:13:36 -0500

    Se agregan sillas

---
+++ reservas.js
@@ -0,0 +1,11 @@
+let contenedor = document.getElementById ("container")
+
+contenedor.addEventListener("click",function(event){
+
+    if(event.target.classList.contains="w-100"){
+
+        event.target.src="img/cossio croma.jpg"
+       
+    }
+
+})
