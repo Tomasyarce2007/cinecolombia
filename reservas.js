08bad94ebc5e7f15305fba9ae8c6408deda3eb81
tomasyarce2007 <toyartej@gmail.com>
2022-03-22 17:00:06 -0500

    cine colombia pro

--- reservas.js
+++ reservas.js
@@ -1,11 +1,40 @@
 let contenedor = document.getElementById ("container")
-
+let bandera=true
 contenedor.addEventListener("click",function(event){
-
+if(bandera==true){
     if(event.target.classList.contains="w-100"){
+        event.target.src="img/cinema2.png"
+        bandera=false
 
-        event.target.src="img/cossio croma.jpg"
-       
+    }
+}
+
+    else{
+        if(event.target.classList.contains("w-100")){
+            event.target.src="img/cinema-chair.png"
+            bandera=true
+        }
     }
 
 })
+
+let boton = document.getElementById ("boton")
+boton.addEventListener("click", recibirDatos)
+function recibirDatos (event){
+    event.preventDefault()
+    
+    let cantidad = document.getElementById ("cantidad").value
+    
+    
+
+    let asiento = document.getElementById ("asiento").value
+
+    if(asiento == "preferencial"){
+        alert("El valor a pagar es de $" + cantidad*40000)
+    } 
+
+    else{
+        alert("El valor a pagar es de $" + cantidad*20000)
+    }
+
+}
